d77df2d523935acdf6f820f90fd22d14c3e78b65
Justin Joseph <lalalolowhatever@gmail.com>
2024-12-13 23:39:04 +0530

    a

---
+++ Calculator/Calculator.java
@@ -0,0 +1,135 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+class Calculator implements ActionListener{
+    double a = 0, b = 0, result = 0;
+    int symbol = -1;
+    boolean remove = false;
+    String str = "";
+    JFrame frame = new JFrame();
+    JPanel panel = new JPanel();
+    JTextField field = new JTextField();
+    JButton numButton[] = new JButton[10];
+    JButton opButton[] = new JButton[6];
+    Calculator(){
+        frame.setTitle("Simple Calculator");
+        frame.setResizable(false);
+        panel.setLayout(new GridLayout(4, 4, 4, 4));
+        frame.getContentPane().setBackground(new Color(254, 215, 191));
+        panel.setBackground(new Color(254, 215, 191));
+        for(int i = 0; i < 10; i++){
+            numButton[i] = new JButton(String.valueOf(i));
+            numButton[i].setFocusable(false);
+            numButton[i].addActionListener(this);
+            numButton[i].setBackground(new Color(228, 175, 176));
+            numButton[i].setForeground(new Color(154, 119, 135));
+        }
+        opButton[0] = new JButton("+");
+        opButton[1] = new JButton("-");
+        opButton[2] = new JButton("*");
+        opButton[3] = new JButton("%");
+        opButton[4] = new JButton("DEL");
+        opButton[5] = new JButton("=");
+        for(int i = 0; i < 6; i++){
+            opButton[i].setFocusable(false);
+            opButton[i].addActionListener(this);
+            opButton[i].setBackground(new Color(228, 175, 176));
+            opButton[i].setForeground(new Color(154, 119, 135));
+        }
+        field.setFont(new Font("Arial", Font.PLAIN, 40));
+        field.setEditable(false);
+        field.setFocusable(false);
+        field.setBounds(25, 30, 335, 80);
+        field.setForeground(new Color(154, 119, 135));
+        panel.setBounds(25, 120, 335, 400);
+        panel.add(numButton[9]);
+        panel.add(numButton[8]);
+        panel.add(numButton[7]);
+        panel.add(opButton[0]);
+        panel.add(numButton[6]);
+        panel.add(numButton[5]);
+        panel.add(numButton[4]);
+        panel.add(opButton[1]);
+        panel.add(numButton[3]);
+        panel.add(numButton[2]);
+        panel.add(numButton[1]);
+        panel.add(opButton[2]);
+        panel.add(opButton[4]);
+        panel.add(numButton[0]);
+        panel.add(opButton[5]);
+        panel.add(opButton[3]);
+        frame.add(panel);
+        frame.add(field);
+        frame.setBounds(400, 50, 400, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
+        frame.setVisible(true);
+    }
+    public static void main(String[] args){
+        new Calculator();
+    }
+    @Override
+    public void actionPerformed(ActionEvent e){
+        if(remove){
+            remove = false;
+            field.setForeground(new Color(154, 119, 135));
+            field.setText("");
+        }
+        for(int i = 0; i < 10; i++){
+            if(e.getSource() == numButton[i]){
+                field.setText(field.getText()+numButton[i].getText());
+            }
+        }
+        for(int i = 0; i < 4; i++){
+            if(e.getSource() == opButton[i]){
+                a=Double.parseDouble(field.getText());
+                field.setText("");
+                symbol=i;
+            }
+        }
+        if(e.getSource() == opButton[4]){
+            for(int i = 0; i < field.getText().length()-1; i++){
+                str=str+String.valueOf(field.getText().charAt(i));
+            }
+            field.setText("");
+            str = "";
+        }
+        if(e.getSource() == opButton[5]){
+            switch(symbol){
+                case 0:
+                    b = Double.parseDouble(field.getText());
+                    result = a+b;
+                    field.setText(String.valueOf(result));
+                    break;
+                case 1:
+                    b = Double.parseDouble(field.getText());
+                    result = a-b;
+                    field.setText(String.valueOf(result));
+                    break;
+                case 2:
+                    b = Double.parseDouble(field.getText());
+                    result = a*b;
+                    field.setText(String.valueOf(result));
+                    break;
+                case 3:
+                    b = Double.parseDouble(field.getText());
+                    if(b != 0){    
+                        result = a/b;
+                        field.setText(String.valueOf(result));
+                    }
+                    else{
+                        field.setForeground(Color.RED);
+                        field.setText("Division by 0");
+                        remove = true;
+                    }
+                    break;
+                default:
+                    break;
+            }
+            a = 0;
+            b = 0;
+            result = 0;
+            symbol = -1;
+        }
+    }
+}
