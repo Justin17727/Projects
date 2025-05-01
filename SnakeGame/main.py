31e1ad8b859baeaab0f4a0b59c4909f039eed994
Justin Joseph <lalalolowhatever@gmail.com>
2025-05-01 22:15:22 +0530

    fixed wonkiness of snake and movements

--- SnakeGame/main.py
+++ SnakeGame/main.py
@@ -52,19 +52,19 @@ def update(snake_coordinates: list[list[int]], x_direction: int, y_direction: in
 def on_key_press(event) -> bool:
     # use it to set directions
     global x, y
-    if event.keysym == 'Up':
+    if event.keysym == 'Up' and y != -1:
         x = 0
         y = 1
         return True
-    if event.keysym == 'Down':
+    if event.keysym == 'Down' and y != 1:
         x = 0
         y = -1
         return True
-    if event.keysym == 'Right':
+    if event.keysym == 'Right' and x != -1:
         x = 1
         y = 0
         return True
-    if event.keysym == 'Left':
+    if event.keysym == 'Left' and x != 1:
         x = -1
         y = 0
         return True
@@ -141,10 +141,17 @@ def snake_growth(snake_coordinates: list[list[int]], snake_frame: list[tk.Frame]
             snake_coordinates.append([loc_x - 1, loc_y])
 
 
+def score_display():
+    # display score
+
+    pass
+
+
 def apple_logic(snake_coordinates: list[list[int]], apple_coordinates: list[list[int]]) -> None:
     # check if apple is eaten and then respawn an apple
-    global loc
+    global loc, score
     if apple_coordinates[0] == snake_coordinates[0]:
+        score += 1
         loc.clear()
         loc.append(place_apple())
         snake_growth(snake_coordinates, snake)
@@ -167,29 +174,30 @@ x: int = 0
 y: int = 1
 a: int = 25
 b: int = 25
+score: int = 0
 root: tk.Tk = tk.Tk()
 snake: list[tk.Frame] = []
 snek: list[list[int]] = []
-apple: tk.Frame = tk.Frame(root, width=1, height=1, bg='red')
+apple: tk.Frame = tk.Frame(root, width=10, height=10, bg='red')
 apple_loc: list[list[int]] = []
 for i in range(50):
     for j in range(50):
         apple_loc.append([i, j])
 loc: list[list[int]] = []
 for m in range(25):
-    snake.append(tk.Frame(root, width=1, height=1, bg='green'))
+    snake.append(tk.Frame(root, width=10, height=10, bg='green'))
     snek.append([a + m, b])
     apple_loc.remove([a + m, b])
 root.geometry('500x500')
 for i in range(0, 50):
-    root.grid_rowconfigure(i, weight=10, pad=0)
-    root.grid_columnconfigure(i, weight=10, pad=0)
+    root.grid_rowconfigure(i, minsize=10)
+    root.grid_columnconfigure(i, minsize=10)
 for m in range(25):
     snake[m].grid(row=a + m, column=b)
 root.bind('<KeyPress>', on_key_press)
 root.title('Snake Game')
 root.minsize(height=500, width=500)
-root.maxsize(height=500, width=500)
+root.resizable(False, False)
 loc.append(place_apple())
 root.after(120, work, root)
 root.mainloop()
